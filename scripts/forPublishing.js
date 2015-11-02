f4a6e15658814dac06e6ab0a3b1a32eda94a759c
Bruce McPherson <bruce@mcpher.com>
2015-11-02 14:05:09 +0000

    updated by GasGit automation

---
+++ scripts/forPublishing.js
@@ -0,0 +1,27 @@
+
+function showMyScriptAppResource(s) {
+  try {
+    return ScriptApp.getResource(s);
+  }
+  catch (err) {
+    throw err + " getting script " + s;
+  }
+}
+
+
+function getLibraryInfo () {
+
+  return { 
+    info: {
+      name:'cGoa',
+      version:'0.0.1',
+      key:'MZx5DzNPsYjVyZaR67xXJQai_d-phDA33',
+      description:'simple library for google oauth2',
+      share:'https://script.google.com/d/14sGrM0uhamXv89jexZByhH55fjuC7JA6mooKN52b6vendfTX5OFRgCi7/edit?usp=sharing'
+    },
+    dependencies:[
+      cUseful.getLibraryInfo(),
+      cServiceAuth.getLibraryInfo()
+    ]
+  }; 
+}
